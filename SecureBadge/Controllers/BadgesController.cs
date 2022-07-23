3e9897aff4fdc2e8d3bab51222d804f259f0c2b1
Duncan Ferber <duncan.ferber@ascendlearning.com>
2022-07-23 14:02:28 -0500

    adding in badges controller

---
+++ SecureBadge/Controllers/BadgesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureBadge.Controllers
+{
+    public class BadgesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
