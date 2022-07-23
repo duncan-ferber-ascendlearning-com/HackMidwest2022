9dc903c672cb9483b1aa1d113865ea6f038edc13
Jason Brown <jason.brown@ascendlearning.com>
2022-07-23 15:08:58 -0500

    Add Badges View/Model/Controller
    Add Badges View/Model/Controller

---
+++ SecureBadge/Entities/Models/BadgesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureBadge.Entities.Models
+{
+    public class BadgesModel
+    {
+        public int ID { get; set; }
+    }
+}
