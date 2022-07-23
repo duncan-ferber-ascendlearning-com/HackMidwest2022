97d8e1c5a0bc5e7d9c6bc2972215e1dd36a4ba7a
Jason Brown <jason.brown@ascendlearning.com>
2022-07-23 13:59:11 -0500

    Setup Entity Framework
    Entity Setup

---
+++ SecureBadge/Entities/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureBadge.Entities
+{
+    public class ApplicationContext :IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
